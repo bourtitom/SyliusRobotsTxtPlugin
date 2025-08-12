92e76414b370f84796f60308185323845d2f7d00
Tom Bourti <t.bourti@monsieurbiz.com>
2025-08-12 16:02:15 +0200

    fix: test phpcs

--- src/MonsieurBizSyliusRobotsTxtPlugin.php
+++ src/MonsieurBizSyliusRobotsTxtPlugin.php
@@ -11,15 +11,13 @@ declare(strict_types=1);
 
 namespace MonsieurBiz\SyliusRobotsTxtPlugin;
 
-use LogicException;
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class MonsieurBizSyliusRobotsTxtPlugin extends Bundle
 {
     use SyliusPluginTrait;
-    
+
     public function getPath(): string
     {
         return \dirname(__DIR__);
